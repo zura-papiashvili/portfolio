9ed9716dfb30cef429a42eba13e9f50e9bedec55
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2025-01-30 19:06:41 -0500

    feat: languages

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 from django.core.exceptions import ValidationError
 from .utils import compress_and_optimize_image
+from django.utils.translation import gettext_lazy as _
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Product Name"))
 
 
 # Create your models here.
@@ -16,28 +21,28 @@ def validate_image_size(image):
 
 
 class Tag(models.Model):
-    caption = models.CharField(max_length=20, verbose_name="etiqueta")
+    caption = models.CharField(max_length=20, verbose_name=_("Tag"))
 
     def __str__(self):
         return self.caption
 
     class Meta:
-        verbose_name = "Etiqueta"
-        verbose_name_plural = "Etiquetas"
+        verbose_name = _("Tag")
+        verbose_name_plural = _("Tags")
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=100, verbose_name="nombre")
-    last_name = models.CharField(max_length=100, verbose_name="apellido")
-    email_address = models.EmailField(verbose_name="correo electrónico")
+    first_name = models.CharField(max_length=100, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=100, verbose_name=_("Last Name"))
+    email_address = models.EmailField(verbose_name=_("Email Address"))
     image = models.ImageField(
         upload_to="authors",
         null=True,
-        verbose_name="imagen",
+        verbose_name=_("Image"),
         validators=[validate_image_size],
     )
     bio = models.TextField(
-        validators=[MinLengthValidator(10)], null=True, verbose_name="biografía"
+        validators=[MinLengthValidator(10)], null=True, verbose_name=_("Biography")
     )
 
     def full_name(self):
@@ -53,87 +58,87 @@ class Author(models.Model):
         super().save(*args, **kwargs)
 
     class Meta:
-        verbose_name = "Autor"
-        verbose_name_plural = "Autores"
+        verbose_name = _("Author")
+        verbose_name_plural = _("Authors")
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=150, verbose_name="título")
-    excerpt = models.CharField(max_length=200, null=True, verbose_name="resumen")
-    date = models.DateField(auto_now=True, verbose_name="fecha")
+    title = models.CharField(max_length=150, verbose_name=_("Title"))
+    excerpt = models.CharField(max_length=200, null=True, verbose_name=_("Excerpt"))
+    date = models.DateField(auto_now=True, verbose_name=_("Date"))
     access_type = models.CharField(
         max_length=20,
-        choices=[("public", "Público"), ("private", "Privado")],
+        choices=[("public", _("Public")), ("private", _("Private"))],
         default="public",
-        verbose_name="tipo de acceso",
+        verbose_name=_("Access Type"),
     )
-    slug = models.SlugField(unique=True, db_index=True, verbose_name="slug")
-    youtube_url = models.URLField(null=True, verbose_name="URL de YouTube")
+    slug = models.SlugField(unique=True, db_index=True, verbose_name=_("Slug"))
+    youtube_url = models.URLField(null=True, verbose_name=_("YouTube URL"))
     content = models.TextField(
-        validators=[MinLengthValidator(10)], verbose_name="contenido"
+        validators=[MinLengthValidator(10)], verbose_name=_("Content")
     )
     author = models.ForeignKey(
         Author,
         on_delete=models.SET_NULL,
         null=True,
         related_name="posts",
-        verbose_name="autor",
+        verbose_name=_("Author"),
     )
-    tags = models.ManyToManyField(Tag, related_name="posts", verbose_name="etiquetas")
+    tags = models.ManyToManyField(Tag, related_name="posts", verbose_name=_("Tags"))
 
     class Meta:
-        verbose_name = "Video"
-        verbose_name_plural = "Videos"
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    user_name = models.CharField(max_length=100, verbose_name="nombre de usuario")
-    user_email = models.EmailField(verbose_name="correo electrónico")
-    text = models.TextField(max_length=400, verbose_name="comentario")
+    user_name = models.CharField(max_length=100, verbose_name=_("User Name"))
+    user_email = models.EmailField(verbose_name=_("Email Address"))
+    text = models.TextField(max_length=400, verbose_name=_("Comment"))
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments", verbose_name="video"
+        Post, on_delete=models.CASCADE, related_name="comments", verbose_name=_("Post")
     )
 
     def __str__(self):
         return self.user_name
 
     class Meta:
-        verbose_name = "Comentario"
-        verbose_name_plural = "Comentarios"
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
 
 
-# frequently asked questions
+# Frequently Asked Questions
 
 
 class FAQ(models.Model):
-    question = models.CharField(max_length=100, verbose_name="pregunta")
-    answer = models.TextField(max_length=400, verbose_name="respuesta")
+    question = models.CharField(max_length=100, verbose_name=_("Question"))
+    answer = models.TextField(max_length=400, verbose_name=_("Answer"))
 
     def __str__(self):
         return self.question
 
     class Meta:
-        verbose_name = "Pregunta frecuente"
-        verbose_name_plural = "Preguntas frecuentes"
+        verbose_name = _("FAQ")
+        verbose_name_plural = _("FAQs")
 
 
-#  content for pages
+# Content for pages
 # Carousel model
 class Carousel(models.Model):
-    title = models.CharField(max_length=100, verbose_name="Título")
+    title = models.CharField(max_length=100, verbose_name=_("Title"))
     description = models.TextField(
-        max_length=400, verbose_name="Descripción", blank=True, null=True
+        max_length=400, verbose_name=_("Description"), blank=True, null=True
     )
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = "Carrusel"
-        verbose_name_plural = "Carruseles"
+        verbose_name = _("Carousel")
+        verbose_name_plural = _("Carousels")
 
 
 # CarouselImage model
@@ -142,69 +147,62 @@ class CarouselImage(models.Model):
         Carousel,
         related_name="images",
         on_delete=models.CASCADE,
-        verbose_name="Carrusel",
+        verbose_name=_("Carousel"),
     )
     image = models.ImageField(
         upload_to="carousel/images",
-        verbose_name="Imagen",
-        validators=[
-            validate_image_size
-        ],  # Replace with your actual image validator if needed
+        verbose_name=_("Image"),
+        validators=[validate_image_size],
     )
     caption = models.CharField(
-        max_length=200, verbose_name="Pie de foto", blank=True, null=True
+        max_length=200, verbose_name=_("Caption"), blank=True, null=True
     )
 
-    url = models.URLField(max_length=200, verbose_name="URL", blank=True, null=True)
-
-    # def save(self, *args, **kwargs):
-    #     # Compress and optimize image before saving
-    #     self.image = compress_and_optimize_image(self.image, quality=100)
-    #     super().save(*args, **kwargs)
+    url = models.URLField(max_length=200, verbose_name=_("URL"), blank=True, null=True)
 
     def __str__(self):
-        return f"Imagen de {self.carousel.title}"
+        return f"Image of {self.carousel.title}"
 
     class Meta:
-        verbose_name = "Imagen del Carrusel"
-        verbose_name_plural = "Imágenes del Carrusel"
+        verbose_name = _("Carousel Image")
+        verbose_name_plural = _("Carousel Images")
 
 
 class RestrictedPage(models.Model):
-    title = models.CharField(max_length=100, verbose_name="título")
-    content = models.TextField(verbose_name="contenido")
+    title = models.CharField(max_length=100, verbose_name=_("Title"))
+    content = models.TextField(verbose_name=_("Content"))
     access_code = models.CharField(
-        max_length=20, verbose_name="codigo de acceso", unique=True
+        max_length=20, verbose_name=_("Access Code"), unique=True
     )  # The code for accessing the page
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = "Página para iniciados"
-        verbose_name_plural = "Páginas para iniciados"
+        verbose_name = _("Restricted Page")
+        verbose_name_plural = _("Restricted Pages")
 
 
 class ZoomEvent(models.Model):
-    title = models.CharField(max_length=200, verbose_name="título")
+    title = models.CharField(max_length=200, verbose_name=_("Title"))
     type = models.CharField(
         max_length=20,
-        choices=[("webinar", "Webinar"), ("workshop", "Taller")],
-        verbose_name="tipo",
+        choices=[("webinar", _("Webinar")), ("workshop", _("Workshop"))],
+        verbose_name=_("Type"),
     )
     access_type = models.CharField(
         max_length=20,
-        choices=[("public", "Público"), ("private", "Privado")],
+        choices=[("public", _("Public")), ("private", _("Private"))],
         default="public",
-        verbose_name="tipo de acceso",
+        verbose_name=_("Access Type"),
     )
-    description = models.TextField(verbose_name="descripción")
-    event_time = models.DateTimeField(verbose_name="fecha y hora")
-    zoom_link = models.URLField(verbose_name="enlace de zoom")
+    description = models.TextField(verbose_name=_("Description"))
+    event_time = models.DateTimeField(verbose_name=_("Event Time"))
+    zoom_link = models.URLField(verbose_name=_("Zoom Link"))
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = "Evento de Zoom"
-        verbose_name_plural = "Eventos de Zoom"
+        verbose_name = _("Zoom Event")
+        verbose_name_plural = _("Zoom Events")
